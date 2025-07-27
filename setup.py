5ec6d6d0df3d64b039b69a704630877fcc38de11
yadamkarthik <aiyadamkarthik@gmail.com>
2025-07-27 14:17:26 +0530

    Add setup, Req's and .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_ChatBot",
+    version="0.1.0",
+    author="Yadam Karthik",
+    author_email="aiyadamkarthik@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
